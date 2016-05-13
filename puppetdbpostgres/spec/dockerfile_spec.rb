0d0ff7ec17b8d9adc53a10dce7e2277c46da3f7d
Gareth Rushgrove <gareth@morethanseven.net>
2016-05-13 09:23:56 +0100

    Add basic spec tests for postgres image

---
+++ puppetdbpostgres/spec/dockerfile_spec.rb
@@ -0,0 +1,37 @@
+require 'serverspec'
+require 'docker'
+
+describe 'Dockerfile' do
+  before(:all) do
+    root = File.dirname(File.dirname(__FILE__))
+    image = Docker::Image.build_from_dir(root)
+
+    set :os, family: :debian
+    set :backend, :docker
+    set :docker_image, image.id
+  end
+
+  describe package('postgresql-common') do
+    it { is_expected.to be_installed }
+  end
+
+  describe package('postgresql-9.5') do
+    it { is_expected.to be_installed }
+  end
+
+  describe package('postgresql-contrib-9.5') do
+    it { is_expected.to be_installed }
+  end
+
+  describe command('postgres --help') do
+    its(:exit_status) { should eq 0 }
+  end
+
+  describe group('postgres') do
+    it { should exist }
+  end
+
+  describe user('postgres') do
+      it { should exist }
+  end
+end
