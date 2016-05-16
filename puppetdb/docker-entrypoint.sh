82f02116fc7e727df4a20e2cfe0496b689eb1da2
Gareth Rushgrove <gareth@morethanseven.net>
2016-05-16 10:02:19 +0100

    Setup puppetdb on first boot

---
+++ puppetdb/docker-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -d "/etc/puppetlabs/puppetdb/ssl" ]; then
+  while ! nc -z puppet 8140; do
+    sleep 1
+  done
+  set -e
+  /opt/puppetlabs/bin/puppet agent --verbose --onetime --no-daemonize --waitforcert 120
+  /opt/puppetlabs/server/bin/puppetdb ssl-setup -f
+fi
+
+/opt/puppetlabs/server/bin/puppetdb "$@"
