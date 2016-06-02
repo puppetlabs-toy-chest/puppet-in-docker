91df2419fecbebbc5f3b2beaa0240657acc8d56c
Gareth Rushgrove <gareth@morethanseven.net>
2016-06-02 17:54:01 +0100

    Try and avoid issue with timeout on Travis for builds

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 require 'serverspec'
 require 'docker'
 
+# Travis builds can take time
+Docker.options[:read_timeout] = 7200
+
 # Load any shared examples or context helpers
 Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
