473ed72f903dfea835402c3dbdc701e438574cc0
Gareth Rushgrove <gareth@morethanseven.net>
2016-06-03 11:39:12 +0100

    Allow for ovveriding the PuppetDB server urls
    For most usecases simply making a new image which inherits from the
    puppet/puppetserver image with your own configuration is probably
    suitable. But this demonstrates an alternative approach which is
    particularly useful in some of the current demos.

--- puppetserver-standalone/docker-entrypoint.sh
+++ puppetserver-standalone/docker-entrypoint.sh
@@ -2,4 +2,8 @@
 
 chown -R puppet:puppet /etc/puppetlabs/puppet/ssl
 
+if test -n "${PUPPETDB_SERVER_URLS}" ; then
+  sed -i "s@^server_urls.*@server_urls = ${PUPPETDB_SERVER_URLS}@" /etc/puppetlabs/puppet/puppetdb.conf
+fi
+
 exec /opt/puppetlabs/bin/puppetserver "$@"
