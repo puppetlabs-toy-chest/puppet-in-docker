cf9ad8434d19634b450c293b00367468bf8b42c7
Gareth Rushgrove <gareth@morethanseven.net>
2016-08-12 09:13:51 +0100

    Update to latest Puppet version across all containers

--- puppet-agent-ubuntu/spec/dockerfile_spec.rb
+++ puppet-agent-ubuntu/spec/dockerfile_spec.rb
@@ -5,8 +5,10 @@ CURRENT_DIRECTORY = File.dirname(File.dirname(__FILE__))
 describe 'Dockerfile' do
   include_context 'with a docker image'
 
-  describe package('puppet-agent') do
-    it { is_expected.to be_installed }
+  ['puppet-agent', 'wget', 'lsb-release'].each do |package_name|
+    describe package(package_name) do
+      it { is_expected.to be_installed }
+    end
   end
 
   describe file('/opt/puppetlabs/bin/puppet') do
