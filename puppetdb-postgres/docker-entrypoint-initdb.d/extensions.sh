6e23148de6039000a21925becee4daa58d7b342b
Morgan Rhodes <morgan@puppet.com>
2018-05-29 11:34:55 -0700

    (CPR-553) Migrate puppetdb and puppetdb-postgres
    These projects are being migrated to github.com/puppetlabs/puppetdb
    under the 'docker' subdirectory.

--- puppetdb-postgres/docker-entrypoint-initdb.d/extensions.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -x
-set -e
-
-PSQL="psql -U postgres"
-
-$PSQL puppetdb -c "CREATE EXTENSION IF NOT EXISTS pg_trgm;"
-$PSQL puppetdb -c "CREATE EXTENSION IF NOT EXISTS pgcrypto;"
